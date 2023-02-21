01da84011f366ad5faafa6b0fafe95a55b365a0f
Elienai <elienay.soares07@gmail.com>
2023-02-21 11:59:30 -0300

    add store and show user method

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -7,8 +7,9 @@ export default class UsersController {
     const users = await User.all();
     return users;
   }
-  // !create user in front, don't use 
+  // ! Deprecated method, do not use 
   public async create({}: HttpContextContract) {}
+  
   // *create a new user
   public async store({ request }: HttpContextContract) {
     const { name, lastName, email, password, avatar, nickname } = request.body();
@@ -24,8 +25,18 @@ export default class UsersController {
     return user;
   }
   
-  public async show({}: HttpContextContract) {}
-  // !use update method, instead.
+  public async show({ request, response }: HttpContextContract) {
+    const { id } = request.params();
+    const user = await User.findBy('id', id);
+    // todo - [] fix exception when no-uuid is send as parameter;
+    if (!user) {
+      return response.status(204).json({error: 'User not found'});
+    }
+  
+    return user;  
+  }
+
+  // ! Deprecated method, use update method instead.
   public async edit({}: HttpContextContract) {}
   
   public async update({}: HttpContextContract) {}
