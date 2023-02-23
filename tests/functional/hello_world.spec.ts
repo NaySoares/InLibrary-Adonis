4078a97537de41ea4f75d22083df92200899a455
Elienai <elienai@pc-csic-06>
2023-02-23 10:28:46 -0300

    add test user routes

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -1,8 +1,16 @@
-import { test } from '@japa/runner'
+import { test } from '@japa/runner';
+import User from 'App/Models/User';
 
 test('display welcome page', async ({ client }) => {
-  const response = await client.get('/')
+  const response = await client.get('/');
 
-  response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
-})
+  response.assertStatus(200);
+  response.assertBodyContains({ hello: 'world' });
+});
+
+test('Should be able to list all users', async ({ client }) => {
+  const response = await client.get('/users');
+
+  response.assertStatus(200);
+  response.assertBodyContains(User);
+});
